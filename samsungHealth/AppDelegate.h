269ba6bd6e9f27b0131f43961dd56373d5c8d8cd
Hao Ge <gehao868@gmail.com>
2014-07-22 03:06:58 -0400

    Initial Commit

---
+++ samsungHealth/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  samsungHealth
+//
+//  Created by Hao Ge on 7/22/14.
+//  Copyright (c) 2014 Hao Ge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ samsungHealth/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  samsungHealth
+//
+//  Created by Hao Ge on 7/22/14.
+//  Copyright (c) 2014 Hao Ge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
