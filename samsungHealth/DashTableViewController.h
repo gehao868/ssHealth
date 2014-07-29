921c2f646c441cca9fcad329ed36c5a381b93ec5
Hao Ge <gehao868@gmail.com>
2014-07-28 22:20:23 -0400

    update error

---
+++ samsungHealth/DashTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  DashTableViewController.h
+//  samsungHealth
+//
+//  Created by Hao Ge on 7/28/14.
+//  Copyright (c) 2014 Hao Ge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DashTableViewController : UITableViewController
+
+@end
