09e5d0d7d0e7fb193cb53f8fa5bb7c0daeb02e31
zerosylvia <sylviafang0@gmail.com>
2014-07-28 20:30:56 -0400

    Connect ini

---
+++ samsungHealth/FriendListController.h
@@ -0,0 +1,13 @@
+//
+//  FriendListController.h
+//  samsungHealth
+//
+//  Created by Sylvia Fang on 7/28/14.
+//  Copyright (c) 2014 Hao Ge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FriendListController : UIViewController
+
+@end
