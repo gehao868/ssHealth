f1f4751818eeb1ecae366a725e76b36ee3422105
Trey L <lujcmss@gmail.com>
2014-07-30 22:02:42 -0400

    for pull

--- samsungHealth/RootViewController.m
+++ samsungHealth/RootViewController.m
@@ -42,6 +42,157 @@
     // Dispose of any resources that can be recreated.
 }
 
+-(void)facebook
+{
+    self.accountStore = [[ACAccountStore alloc]init];
+    ACAccountType *FBaccountType= [self.accountStore accountTypeWithAccountTypeIdentifier:ACAccountTypeIdentifierFacebook];
+    
+    NSString *key = @"451805654875339";
+    NSDictionary *dictFB = [NSDictionary dictionaryWithObjectsAndKeys:key,ACFacebookAppIdKey,@[@"email"],ACFacebookPermissionsKey, nil];
+    
+    [self.accountStore requestAccessToAccountsWithType:FBaccountType options:dictFB completion:
+     ^(BOOL granted, NSError *e) {
+         if (granted)
+         {
+             NSArray *accounts = [self.accountStore accountsWithAccountType:FBaccountType];
+             //it will always be the last object with single sign on
+             self.facebookAccount = [accounts lastObject];
+             
+             
+             ACAccountCredential *facebookCredential = [self.facebookAccount credential];
+             NSString *accessToken = [facebookCredential oauthToken];
+             NSLog(@"Facebook Access Token: %@", accessToken);
+             
+             
+             NSLog(@"facebook account =%@",self.facebookAccount);
+             
+             [self get];
+             
+             [self getFBFriends];
+             
+             isFacebookAvailable = 1;
+         } else
+         {
+             //Fail gracefully...
+             NSLog(@"error getting permission yupeeeeeee %@",e);
+             sleep(10);
+             NSLog(@"awake from sleep");
+             isFacebookAvailable = 0;
+             
+         }
+     }];
+    
+    
+    
+}
 
-//
+-(void)checkfacebookstatus
+{
+    if (isFacebookAvailable == 0)
+    {
+        [self checkFacebook];
+        isFacebookAvailable = 1;
+    }
+    else
+    {
+        printf("Get out from our game");
+    }
+}
+
+
+-(void)get
+{
+    
+    NSURL *requestURL = [NSURL URLWithString:@"https://graph.facebook.com/me"];
+    
+    SLRequest *request = [SLRequest requestForServiceType:SLServiceTypeFacebook requestMethod:SLRequestMethodGET URL:requestURL parameters:nil];
+    request.account = self.facebookAccount;
+    
+    [request performRequestWithHandler:^(NSData *data, NSHTTPURLResponse *response, NSError *error) {
+        
+        if(!error)
+        {
+            
+            NSDictionary *list =[NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:&error];
+            
+            NSLog(@"Dictionary contains: %@", list );
+            
+            
+            
+            
+            self.globalmailID   = [NSString stringWithFormat:@"%@",[list objectForKey:@"email"]];
+            NSLog(@"global mail ID : %@",globalmailID);
+            
+            
+            fbname = [NSString stringWithFormat:@"%@",[list objectForKey:@"name"]];
+            NSLog(@"faceboooookkkk name %@",fbname);
+            
+            
+            
+            
+            if([list objectForKey:@"error"]!=nil)
+            {
+                [self attemptRenewCredentials];
+            }
+            dispatch_async(dispatch_get_main_queue(),^{
+                
+            });
+        }
+        else
+        {
+            //handle error gracefully
+            NSLog(@"error from get%@",error);
+            //attempt to revalidate credentials
+        }
+        
+    }];
+    
+    self.accountStore = [[ACAccountStore alloc]init];
+    ACAccountType *FBaccountType= [self.accountStore accountTypeWithAccountTypeIdentifier:ACAccountTypeIdentifierFacebook];
+    
+    NSString *key = @"451805654875339";
+    NSDictionary *dictFB = [NSDictionary dictionaryWithObjectsAndKeys:key,ACFacebookAppIdKey,@[@"friends_videos"],ACFacebookPermissionsKey, nil];
+    
+    
+    [self.accountStore requestAccessToAccountsWithType:FBaccountType options:dictFB completion:
+     ^(BOOL granted, NSError *e) {}];
+    
+}
+
+
+
+
+-(void)accountChanged:(NSNotification *)notification
+{
+    [self attemptRenewCredentials];
+}
+
+-(void)attemptRenewCredentials
+{
+    [self.accountStore renewCredentialsForAccount:(ACAccount *)self.facebookAccount completion:^(ACAccountCredentialRenewResult renewResult, NSError *error){
+        if(!error)
+        {
+            switch (renewResult) {
+                case ACAccountCredentialRenewResultRenewed:
+                    NSLog(@"Good to go");
+                    [self get];
+                    break;
+                case ACAccountCredentialRenewResultRejected:
+                    NSLog(@"User declined permission");
+                    break;
+                case ACAccountCredentialRenewResultFailed:
+                    NSLog(@"non-user-initiated cancel, you may attempt to retry");
+                    break;
+                default:
+                    break;
+            }
+            
+        }
+        else{
+            //handle error gracefully
+            NSLog(@"error from renew credentials%@",error);
+        }
+    }];
+}
+
 @end
