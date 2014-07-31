98f839d67d7ac8e56aa3ac60a1ec8fb47f80afa3
Trey L <lujcmss@gmail.com>
2014-07-31 18:31:54 -0400

    get friends who use our App

--- samsungHealth/NavigationController.m
+++ samsungHealth/NavigationController.m
@@ -39,7 +39,7 @@
     } else {
         // Open a session showing the user the login UI
         // You must ALWAYS ask for public_profile permissions when opening a session
-        [FBSession openActiveSessionWithReadPermissions:@[@"public_profile", @"email", @"read_friendlists", @"user_friends"] allowLoginUI:YES completionHandler:
+        [FBSession openActiveSessionWithReadPermissions:@[@"public_profile", @"email", @"user_friends"] allowLoginUI:YES completionHandler:
          ^(FBSession *session, FBSessionState state, NSError *error) {
              
              // Retrieve the app delegate
