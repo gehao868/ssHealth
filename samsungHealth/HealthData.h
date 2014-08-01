829cc2e23f0d1cc9b48a75f9d7112acbbffd1880
Hao Ge <gehao868@gmail.com>
2014-08-01 19:32:51 -0400

    update data group

---
+++ samsungHealth/HealthData.h
@@ -0,0 +1,13 @@
+//
+//  HealthData.h
+//  samsungHealth
+//
+//  Created by Hao Ge on 8/1/14.
+//  Copyright (c) 2014 Hao Ge. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HealthData : NSObject
+
+@end
---
+++ samsungHealth/HealthData.m
@@ -0,0 +1,13 @@
+//
+//  HealthData.m
+//  samsungHealth
+//
+//  Created by Hao Ge on 8/1/14.
+//  Copyright (c) 2014 Hao Ge. All rights reserved.
+//
+
+#import "HealthData.h"
+
+@implementation HealthData
+
+@end
