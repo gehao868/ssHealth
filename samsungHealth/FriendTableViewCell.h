df55f5eb5eb1586f75a55e983c9fc536179982a4
Hao Ge <gehao868@gmail.com>
2014-08-02 17:19:58 -0400

    update connect

---
+++ samsungHealth/FriendTableViewCell.h
@@ -0,0 +1,15 @@
+//
+//  FriendTableViewCell.h
+//  samsungHealth
+//
+//  Created by Hao Ge on 8/2/14.
+//  Copyright (c) 2014 Hao Ge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FriendTableViewCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UILabel *name;
+@property (weak, nonatomic) IBOutlet UIImageView *pic;
+
+@end
