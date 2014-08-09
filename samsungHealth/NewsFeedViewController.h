46e4cac76e8c1bf048efea4f2d5833f129c0fb98
Trey L <lujcmss@gmail.com>
2014-08-08 20:02:13 -0400

    drop down

---
+++ samsungHealth/NewsFeedViewController.h
@@ -0,0 +1,14 @@
+//
+//  NewsFeedViewController.h
+//  samsungHealth
+//
+//  Created by Trey_L on 8/8/14.
+//  Copyright (c) 2014 Hao Ge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "SINavigationMenuView.h"
+
+@interface NewsFeedViewController : UIViewController <SINavigationMenuDelegate>
+
+@end
