24ea28c37288052e5059acba902c4932ceab3e28
Hao Ge <gehao868@gmail.com>
2014-08-08 20:02:30 -0400

    update goal

---
+++ samsungHealth/Goal.h
@@ -0,0 +1,17 @@
+//
+//  Goal.h
+//  samsungHealth
+//
+//  Created by Hao Ge on 8/8/14.
+//  Copyright (c) 2014 Hao Ge. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Goal : NSObject
+
+@property (nonatomic) NSInteger *type;
+@property (nonatomic) NSInteger *quota;
+@property (nonatomic) NSString *des;
+
+@end
